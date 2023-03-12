1de018f914ac67bdc565110464be46669ca26e91
ViktorijaVer <viktorija.verbickaite@gmail.com>
2023-03-12 23:19:51 +0200

    init: eslint configuration

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
-import * as dotenv from 'dotenv'
-dotenv.config();
+import * as dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
+
+dotenv.config();
 
 const { SERVER_PORT, SERVER_DOMAIN } = process.env;
 
 if (SERVER_PORT === undefined || SERVER_DOMAIN === undefined) {
-    throw new Error("Please define constants in .env file");
+  throw new Error('Please define constants in .env file');
 }
 
 const server = express();
@@ -15,5 +16,5 @@ server.use(morgan('tiny'));
 server.use(express.static('public'));
 
 server.listen(SERVER_PORT, () => {
-    console.log(`server is runing on: http://${SERVER_DOMAIN}:${SERVER_PORT}`)
-})
+  console.log(`server is runing on: http://${SERVER_DOMAIN}:${SERVER_PORT}`);
+});

b6eb16e71407ecca453fb4163376fe9afbf3659c
ViktorijaVer <viktorija.verbickaite@gmail.com>
2023-03-12 22:59:29 +0200

    init: new libraries were added

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,19 @@
 import * as dotenv from 'dotenv'
 dotenv.config();
 import express from 'express';
+import morgan from 'morgan';
 
 const { SERVER_PORT, SERVER_DOMAIN } = process.env;
 
-if(SERVER_PORT === undefined || SERVER_DOMAIN === undefined) {
+if (SERVER_PORT === undefined || SERVER_DOMAIN === undefined) {
     throw new Error("Please define constants in .env file");
 }
 
 const server = express();
+
+server.use(morgan('tiny'));
 server.use(express.static('public'));
 
-server.listen(5001, () => {
+server.listen(SERVER_PORT, () => {
     console.log(`server is runing on: http://${SERVER_DOMAIN}:${SERVER_PORT}`)
 })
